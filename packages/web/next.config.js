2fb6c254bbe4904781e32da8c9edc4f86d35551b
JeongwooHam <hamjw0122@gmail.com>
2024-01-18 22:20:05 +0900

    🐛 fix: styleX 관련 모듈 에러 해결

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+import stylexPlugin from "@stylexjs/nextjs-plugin";
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+};
+
+module.exports = stylexPlugin({
+  filename: "stylex-bundle.css",
+  rootDir: __dirname,
+  useCSSLayers: true,
+})(nextConfig);
